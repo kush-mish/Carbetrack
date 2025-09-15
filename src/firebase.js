4c71f52b47854ed40bf3bd94a69eaf374ccbb5b5
Kushagra Misra <misra.kushagra@yahoo.in>
2025-09-15 14:17:36 +0530

    add Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Y4cXdBzAgFj3wPEiK0LlQkr0WzPA078",
+  authDomain: "carbetrack.firebaseapp.com",
+  projectId: "carbetrack",
+  storageBucket: "carbetrack.firebasestorage.app",
+  messagingSenderId: "927410756622",
+  appId: "1:927410756622:web:1ad63683ece827882a705f"
+};
+
+const app = initializeApp(firebaseConfig);
